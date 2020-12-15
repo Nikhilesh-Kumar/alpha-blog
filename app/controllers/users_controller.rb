5ff89b0d333e08020c13c47f71c159631db249a0
alpha-blog app <nikhileshkumar2509@gmail.com>
2020-12-15 15:28:42 +0530

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,13 @@ class UsersController < ApplicationController
 
    def create
     @user = User.new(user_params)
-    if @user.save
-    flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-    redirect_to articles_path
-    else
-    render 'new'
-    end
+      if @user.save
+         session[:user_id] = @user.id
+         flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+         redirect_to user_path(@user)
+      else
+         render 'new'
+      end
    end
 
    def edit
@@ -57,6 +58,6 @@ class UsersController < ApplicationController
          redirect_to root_path
       end
    end
-   
+
 
 end
